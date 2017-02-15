a229672030e7dbf2dc7bd679adbd900908bfcf54
Larry <tradesouthwest@outlook.com>
2017-02-15 14:32:18 -0700

    new version
    copy paste page

--- modify-visual-editor.php
+++ modify-visual-editor.php
@@ -1,10 +1,10 @@
 <?php
 /**
 Plugin Name: Modify Visual Editor
-Plugin URI: http://
+Plugin URI: http://themes.tradesouthwest.com/wordpress/plugins/Modify-Visual-Editor/
 Description: This plugin modifies the visual editor for selected users and adds some dialogue to the  editor to deter code deletion or tag stripping. Also includes a function to remove-admin-bar for all users except administrators.
 Author: Larry Judd Oliver @tradesouthwest
-Version: 0.1.0
+Version: 0.2.2
 Author URI: http://
 Text Domain: mved
 Domain Path: /languages
@@ -18,32 +18,77 @@ if ( ! is_admin() ) return; // Only load plugin when user is in admin
 //wp_cache_delete ( 'alloptions', 'options' );
 //Predefined Constants
 
-define( 'MVED_VERSION', '0.1.0' );
-define( 'MVED_URI', '' );
+define( 'MVED_VERSION', '0.2.2' );
+//define( 'MVED_URI', '' );
 
 //activate/deactivate hooks
-function modify_visual_editor_activation() {
+function modify_visual_editor_activation()
+{
+    global $wp_version;
+    $wp = '3.8';
+
+    if ( version_compare( $wp_version, $wp, '<' ) ) {
+
+	   deactivate_plugins( basename( __FILE__ ) );
+        wp_die(		'<p>' .
+			sprintf(
+__( 'This plugin can not be activated because it requires a WordPress version greater than %1$s. Please go to Dashboard &#9656; Updates to the latest version of WordPress .', 'mved' ),
+			$php
+			)
+			. '</p> <a href="' . admin_url( 'plugins.php' ) . '">' . __( 'go back', 'mved' ) . '</a>'
+		);
+        }
 }
 
-//house keeeping after uninstall
-function modify_visual_editor_deactivation() {
 
+//house keeeping fallback
+function modify_visual_editor_deactivation()
+{
     //clean up database and option cache
-    wp_cache_delete ( 'alloptions', 'options' );
-
     //remove hook in profile_update
-    remove_action( 'profile_update','mved_update_user_editor_status', 21 );
 
-    //return to normal
-    mved_update_user_editor_status( '', 'true' );
+    //turn back on 'rich_editing' =true
+   	global $wpdb;
+	$wpdb->query(" UPDATE `" . $wpdb->prefix .
+       "usermeta`  SET `meta_value` = 'true'
+                   WHERE `meta_key` = 'rich_editing' ");
+            return false;
 
-    //add update just once
-    add_action( 'profile_update','mved_update_user_editor_status' );
+
+        remove_action( 'profile_update',
+                       'modify_visual_editor_removeUserStatus', 24 );
+        remove_action( 'personal_options_update',
+                       'modify_visual_editor_removeUserStatus', 25 );
 }
+
+function modify_visual_editor_uninstall()
+{
+    //clean up database and option cache
+    //remove hook in profile_update
+
+    //turn back on 'rich_editing' =true
+   	global $wpdb;
+	$wpdb->query(" UPDATE `" . $wpdb->prefix .
+       "usermeta`  SET `meta_value` = 'true'
+                   WHERE `meta_key` = 'rich_editing' ");
+            return false;
+
+
+        remove_action( 'profile_update',
+                       'modify_visual_editor_removeUserStatus', 24 );
+        remove_action( 'personal_options_update',
+                       'modify_visual_editor_removeUserStatus', 25 );
+
+        delete_option( 'mved_pluginPage' );
+        delete_option( 'mvedASelect' );
+        delete_option( 'mvedCSelect' );
+}
+
 
 /** enqueue scripts
  */
-function modify_visual_editor_enqueue_admin_scripts() {
+function modify_visual_editor_enqueue_admin_scripts()
+{
     wp_enqueue_style( 'mved-admin-style', plugins_url( basename( __DIR__ )) .
                     '/assets/mved-admin-style.css', array(), MVED_VERSION );
 
@@ -55,21 +100,15 @@ add_action( 'admin_enqueue_scripts', 'modify_visual_editor_enqueue_admin_scripts
  * example usage: German MO and PO files should be named
  * mved-domain-de_DE.mo and mved-domain-de_DE.po.
  */
-function modify_visual_editor_load_plugin_textdomain() {
+function modify_visual_editor_load_plugin_textdomain()
+{
     load_plugin_textdomain( 'mved-domain', FALSE, plugins_url( basename( __DIR__ )) . '/languages/' );
 }
-add_action( 'plugins_loaded', 'modify_visual_editor_load_plugin_textdomain' );
+add_action( 'init', 'modify_visual_editor_load_plugin_textdomain' );
 
 
 register_activation_hook(__FILE__, 'modify_visual_editor_activation');
 register_deactivation_hook(__FILE__, 'modify_visual_editor_deactivation');
+register_uninstall_hook(__FILE__, 'modify_visual_editor_uninstall');
 
-/*
-        require_once(ABSPATH . 'wp-includes/pluggable.php');
-        require_once(ABSPATH . 'wp-includes/user.php');
-        require_once(ABSPATH . 'wp-admin/includes/plugin.php');
-        require_once(ABSPATH . 'wp-admin/includes/media.php' );
-*/
-
-require_once( 'mved-modify-media-buttons.php' );
-require_once( 'mved-admin.php' ); 
+require_once( 'mved-admin.php' );
